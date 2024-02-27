f43b17922f073c1325c59175c93450ea4f6a1243
Ece Akın <akinecee3535@hotmail.com>
2024-02-27 18:00:24 +0300

    27.02.2024 18.00

--- MvcProjectCamp/Controllers/AdminCategoryController.cs
+++ MvcProjectCamp/Controllers/AdminCategoryController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRulesFV;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +13,39 @@ namespace MvcProjectCamp.Controllers
 {
     public class AdminCategoryController : Controller
     {
+        CategoryManager cm = new CategoryManager(new EFCategoryDal());
         // GET: AdminCategory
         public ActionResult Index()
         {
+            var categoryValues = cm.GetList();
+
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+         
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category category )
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult validationResult = categoryValidator.Validate(category);
+            if (validationResult.IsValid)
+            {
+                cm.CategoryAddBL(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
             return View();
         }
     }
